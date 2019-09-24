e2f3a8c96a1ce825fc918d268626d5b096f018b3
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2019-09-24 03:09:02 -0400

    Update App.js

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,14 +6,22 @@ const App = () => {
 
   const APP_ID = 'ad8873c5';
   const APP_KEY = "89c1d6870a4b62512c13706d1fad925d";
+  const EXMAPLE_REQ = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`"
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-buttongti ad" type="submit">Search</button>
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="search-button"
+          type="submit">{counter}</button>
       </form>
     </div>
 
