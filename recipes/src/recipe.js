7e4765193706105c05ce8a3559c95586b310f561
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2019-09-24 04:11:47 -0400

    Update recipe.js

--- recipes/src/recipe.js
+++ recipes/src/recipe.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Recipe from './recipe';
+import Recipe from './recipe.js';
 
 const App = () => {
 
