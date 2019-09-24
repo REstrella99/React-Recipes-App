1ba0d73218af549e9039f7e81aef10c0b4539704
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2019-09-24 04:12:47 -0400

    Update recipe.js

--- recipes/src/recipe.js
+++ recipes/src/recipe.js
@@ -1,45 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Recipe from './recipe.js';
+import React from 'react';
 
-const App = () => {
-
-  const APP_ID = "7ebe31e1";
-  const APP_KEY = "e3e4a2a969825b88484787db4cb34c7a";
-
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-
-  }, []);
-
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+const Recipe = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Calories</p>
+            <img src="" alt="" />
+        </div>
     );
-    const data = await response.json();
-    setRecipes(data.hits);
-  };
 
-
-  return (
-    <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
-        <button
-          className="search-button"
-          type="submit">Search</button>
-      </form>
-      {recipes.map(recipe => (
-        <Recipe />
-      ))}
-      ;
-    </div>
-
-
-
-  );
 };
 
-export default App;
+export default Recipe;
