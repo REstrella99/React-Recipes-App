20c43df7f57984e8368ee7d91d3d84c891d70960
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2019-09-24 03:00:41 -0400

    Update App.js

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'ad8873c5';
+  const APP_KEY = "89c1d6870a4b62512c13706d1fad925d";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-buttongti ad" type="submit">Search</button>
+      </form>
     </div>
+
+
+
   );
-}
+};
 
 export default App;
